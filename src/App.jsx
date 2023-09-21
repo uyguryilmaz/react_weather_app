2258622ad4251094dd94d652e6046f2692a6657c
Arrivalx <uygurnex@gmail.com>
2023-09-21 13:41:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null)
+  const [location, setLocation] = useState('')
+
+  useEffect(() =>{
+    const fetchData = async () =>{
+      
+    }
+  })
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
